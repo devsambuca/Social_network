7e0f5e18700c8c452ea35e1d7e273981aa203a83
devsambuca <stoyakovi4@gmail.com>
2019-07-30 07:28:44 +0300

    Added new folder for our components

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>Frontend-developer Fominykh Vladimir</div>;
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img src="" alt=""/>
+      </div>
+      <div className="header-name">DevNet</div>
+      <div className="header-nav"></div>
+
+      
+      </div>
+  );
+};
+
+export default Header;
